19b12dff362e78402c09b180a7df01a5f42ae6ee
rsanders001 <rsanders2k@gmail.com>
2020-05-12 12:27:35 -0500

    Quiz Commit

---
+++ Quiz3/Quiz3/Quiz3/MainPage.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Quiz3
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+
+        string inputName = "";
+
+        List<string> foodList = new List<string>();
+
+        public MainPage()
+        {
+            InitializeComponent();
+
+        }
+
+        private void Name(object sender, EventArgs e)
+        {
+            inputName = responseName.Text;
+            DisplayFoodList.Text = "";
+            for (int i = 0; i < foodList.Count; i++)
+            {
+                var output = inputName + " Likes " + foodList[i];
+                DisplayFoodList.Text += "\n" + output;
+            }
+        }
+
+        private void DisplayListFood(object sender, EventArgs e)
+        {
+            foodList.Add(responseFood.Text);
+            DisplayFoodList.Text = "";
+            for (int i = 0; i < foodList.Count; i++)
+            {
+                var output = inputName + " Likes " + foodList[i];
+                DisplayFoodList.Text += "\n" + output;
+            }
+        }
+    }
+}
